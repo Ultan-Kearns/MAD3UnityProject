db4f11a87275150b2a94354b6dbacca98b9fc37e
unknown <ultankearns@gmail.com>
2019-10-01 16:56:03 +0100

    Redesigning project get movement and added ground now work on platforms and bullet spawners

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //variable for player speed
+    public float speed = 3f;
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();   
+    }
+    private void Move()
+    {
+        //have an input from user
+        var deltaX = Input.GetAxis("Horizontal");
+        //Add the position where player is going to be and give them speed
+        var newXPos = deltaX * speed;
+        rb.velocity = new Vector2(newXPos, rb.velocity.x);
+ 
+
+    }
+}
