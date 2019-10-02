bdbe12790ef48120fa40f59ddca1b69c8f30cd86
unknown <ultankearns@gmail.com>
2019-10-03 00:40:53 +0100

    Detecting collisions need to fix error with player falling through ground, maybe set up collision function with ground?

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+             Debug.Log("Collided");
+    }
+}
