1414ff6e618ba50a6671904040526c2a61946cde
unknown <ultankearns@gmail.com>
2019-10-02 22:26:18 +0100

    Trying to get 2dboxcollider detecting collisions for player game object

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,8 +6,10 @@ public class Movement : MonoBehaviour
 {
     //variable for player speed
     public float speed = 3f;
+    public float jump = 5f;
     // Start is called before the first frame update
     Rigidbody2D rb;
+    GameObject player = GameObject.Find("Player");
     void Start()
     {
 
@@ -17,16 +19,27 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();   
+        PlayerMove();
+        PlayerJump(null);
     }
-    private void Move()
+    private void PlayerMove()
     {
         //have an input from user
         var deltaX = Input.GetAxis("Horizontal");
         //Add the position where player is going to be and give them speed
         var newXPos = deltaX * speed;
-        rb.velocity = new Vector2(newXPos, rb.velocity.x);
- 
-
+        rb.velocity = new Vector2(newXPos, rb.velocity.y);
+    }
+    //need to see if box collider is detecting collistions
+    void PlayerJump(Collision col)
+    {
+        var deltaY = Input.GetAxis("Vertical");
+        var posY = deltaY * jump;
+        if (col.gameObject.name == "player")
+        {
+            rb.velocity = new Vector2(rb.velocity.x, posY);
+        }
     }
 }
+
+
