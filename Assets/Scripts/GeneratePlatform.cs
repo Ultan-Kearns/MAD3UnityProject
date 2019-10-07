d11cd337ffc74a1595c84380f5dfa3c9a16c33cb
unknown <ultankearns@gmail.com>
2019-10-07 21:50:32 +0100

    Working through getting platforms randomly generating

--- Assets/Scripts/GeneratePlatform.cs
+++ Assets/Scripts/GeneratePlatform.cs
@@ -6,21 +6,35 @@ public class GeneratePlatform : MonoBehaviour
 {
     bool test = false;
     // Start is called before the first frame update
+    bool callGenerate = false;
+    int positionX = 10;
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
-    void Update() { 
-  
-    }
+    void Update() {
+        if(callGenerate == false)
+        {
+            testGenerate();
+            callGenerate = true;
+        }
+
+     }
     
     void testGenerate()
     {
         GameObject platform = GameObject.Find("Platform");
-        var clone = Instantiate(platform);
-        clone.transform.position.Set(1, 1, 0);
- 
+        Vector3 newPos = new Vector3(positionX, -5, 0);
+        Quaternion rotation = new Quaternion(0, 0, 0, 0);
+        var clone = Instantiate(platform,newPos,rotation);
+        //use posX variable to set the position of clones
+        //need to find way to update
+        Debug.Log(positionX);
+         if(clone.transform.position.x == -10)
+        {
+            Destroy(clone);
+        }
     }
 }
