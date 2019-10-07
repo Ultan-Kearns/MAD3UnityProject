72dacd858db4089ebfe9edebd08cd15aa769c22f
unknown <ultankearns@gmail.com>
2019-10-07 23:52:01 +0100

    Removed redundant script and added new script to generate bullets

---
+++ Assets/Scripts/GenerateBullets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBullets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void generateBullets()
+    {
+        
+    }
+}
