4757935a013b851546f247ba53e443369662bcbe
unknown <ultankearns@gmail.com>
2019-10-09 19:33:02 +0100

    Drawing gizmos for spawnpoints and added empty script for platform movement

---
+++ Assets/Scripts/PlatformSpawn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawn : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 0.25f);
+    }
+}
+ 
